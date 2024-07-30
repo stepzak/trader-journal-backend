2aab15036bc6ac45cd06c47301a40cb6cc946be4
StepZak <stepanzakatin@gmail.com>
2024-07-30 15:53:44 +0300

    backtest ui

---
+++ client/dist/spa/assets/QCardSection.14da5d36.js
@@ -0,0 +1,1 @@
+import{k as e,f as r,h as n,Z as s}from"./index.13261664.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:a}){const o=r(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:o.value},s(a.default))}});export{i as Q};
---
+++ client/dist/spa/assets/authHeader.c9107c9b.js
@@ -0,0 +1,1 @@
+function t(e="access_token"){return{Authorization:"Bearer "+localStorage.getItem(e)}}export{t as a};
